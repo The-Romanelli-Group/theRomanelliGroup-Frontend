94efe22ee5046cbe51cb0733c72e6bb1901c150e
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-01 14:42:00 +0530

    contact us page redesign v3

--- src/component/page/contactUs/connectForm.js
+++ src/component/page/contactUs/connectForm.js
@@ -237,10 +237,7 @@ return (
                 Connect
             </h2>
 
-            <p className="text-gray-500 mt-3 text-sm leading-6 max-w-xs mx-auto">
-                We'll get back to you within one business day.
-            </p>
-
+    
         </div>
 
         {/* Form */}
