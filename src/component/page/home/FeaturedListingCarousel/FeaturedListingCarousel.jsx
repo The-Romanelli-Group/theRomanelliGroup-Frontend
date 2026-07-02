62a4c87d4e57a3037fba45b15ee3e4deffd2c5e1
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-02 14:04:00 +0530

    caro

--- src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
+++ src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
@@ -103,12 +103,13 @@ console.log(response);
         <div className="flex gap-6">
 
             {properties.map((property) => (
-  <div
-    key={property.ListingKey}
-    className="flex-none w-[88%] sm:w-[48%] xl:w-[31.5%] bg-white text-black p-6"
-  >
-    Test Card
-  </div>
+
+                                <div
+                    key={property.ListingKey}
+                    className="flex-none w-[88%] sm:w-[48%] xl:w-[31.5%]"
+                >
+                <CarouselArrow />
+                </div>
 
             ))}
 
