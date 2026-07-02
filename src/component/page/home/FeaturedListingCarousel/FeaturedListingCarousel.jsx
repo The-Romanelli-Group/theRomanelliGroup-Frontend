bfffea6d1ff426cf8748e9c7a597e2703ec33cd4
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-02 12:45:23 +0530

    carousle

--- src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
+++ src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import apiServices from "../../../../Service/apiService";
-import FeaturedListingCard from "./FeaturedListingCard";
 
 const FeaturedListingCarousel = () => {
 
