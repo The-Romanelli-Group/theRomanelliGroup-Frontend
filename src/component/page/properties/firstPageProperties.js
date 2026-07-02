b8e71660054ea3b2bef573b9c86b08afe0b8b4c4
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-02 22:26:30 +0530

    buttonhright

--- src/component/page/properties/firstPageProperties.js
+++ src/component/page/properties/firstPageProperties.js
@@ -234,7 +234,7 @@ const parseWithGoogle = (searchText) => {
     {/* Buy / Rent */}
 <div className="flex justify-center mb-5">
 
-  <div className="relative inline-flex bg-white rounded-full p-1 shadow-xl">
+  <div className="relative inline-flex bg-white rounded-full p-.5 shadow-xl">
 
     {/* Sliding Pill */}
     <div
