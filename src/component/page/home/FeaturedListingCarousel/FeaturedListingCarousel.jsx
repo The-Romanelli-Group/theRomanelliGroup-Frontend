1802a0b207b73f68ec6902db6fb876890c7841e5
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-02 14:27:54 +0530

    carousel v3

--- src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
+++ src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+ import React, { useEffect, useState } from "react";
 import apiServices from "../../../../Service/apiService";
 import FeaturedListingCard from "./FeaturedListingCard";
 
