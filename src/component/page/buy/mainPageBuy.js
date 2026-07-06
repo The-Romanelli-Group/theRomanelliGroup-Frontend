d325003f3ad5054672fb355d95363a4d0ccd8317
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-06 16:24:21 +0000

    buy page

--- src/component/page/buy/mainPageBuy.js
+++ src/component/page/buy/mainPageBuy.js
@@ -1,69 +1,26 @@
 import FeaturedListingCarousel from "../home/FeaturedListingCarousel/FeaturedListingCarousel";
-import ClientSay from "../Default Pages/Client Review/clientSay";
+import GoogleReviews from "../home/GoogleReviews/GoogleReviews";
 import RoadMap from "./roadMap";
 import QuestionAnswer from "../Default Pages/QuestionAnswer/questionAnswer";
 import Footer from "../Default Pages/footer";
 import Talk from "../Default Pages/talk";
-import { motion } from "framer-motion";
-
-// Animation Variants (Slide-up & Fade-in)
-const fadeInUp = {
-  hidden: { opacity: 0, y: 60 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.6, ease: "easeOut" },
-  },
-};
 
 const MainPageBuy = () => {
   return (
     <div className="font-dmsans">
-      <motion.div
-        variants={fadeInUp}
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
-        className="opacity-0 -my-1"
-      >
-        <RoadMap />
-      </motion.div>
 
-      <motion.div
-        variants={fadeInUp}
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
-        className="opacity-0"
-      >
-        <FeaturedListingCarousel />
-      </motion.div>
+      <RoadMap />
 
-      <motion.div
-        variants={fadeInUp}
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
-        className="opacity-0"
-      >
-        <ClientSay />
-      </motion.div>
+      <FeaturedListingCarousel />
 
-      <motion.div
-        variants={fadeInUp}
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
-        className="opacity-0 -my-1"
-      >
-        <QuestionAnswer onSet={"buyer"} />
-      </motion.div>
+      <GoogleReviews />
 
-      <motion.div
-        variants={fadeInUp}
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
-        className="opacity-0 -my-1"
-      >
-        <Talk />
-        <Footer />
-      </motion.div>
+      <QuestionAnswer onSet={"buyer"} />
+
+      <Talk />
+
+      <Footer />
+
     </div>
   );
 };
