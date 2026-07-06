d0b8b66379a6787dc52761367a7aa135525abd20
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-06 05:06:09 +0000

    hot

--- src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
+++ src/component/page/home/FeaturedListingCarousel/FeaturedListingCarousel.jsx
@@ -61,7 +61,7 @@ console.log("Properties:", response.value);
       <div className="max-w-7xl mx-auto px-6">
 
        <h2 className="text-5xl font-semibold mb-10">
-    Hot in the Market
+    Hot on the Market
 </h2>
 
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
