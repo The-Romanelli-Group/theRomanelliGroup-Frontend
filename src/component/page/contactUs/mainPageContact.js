413b3e049df598a059e79ddcacdd15f2fc73fc60
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-13 17:29:08 +0530

    2

--- src/component/page/contactUs/mainPageContact.js
+++ src/component/page/contactUs/mainPageContact.js
@@ -1,30 +1,28 @@
-import Connected from './connected'
-import Faces from '../Default Pages/faces'
-import Families from '../Default Pages/families'
-import ClientSay from '../Default Pages/Client Review/clientSay'
-import QuestionAnswer from '../Default Pages/QuestionAnswer/questionAnswer'
-import Footer from '../Default Pages/footer'
-import { motion } from "framer-motion";
-
-const fadeInUp = {
-  hidden: { opacity: 0, y: 60 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.2, ease: "easeOut" },
-  },
-};
+import Connected from "./connected";
+import Faces from "../Default Pages/faces";
+import Families from "../Default Pages/families";
+import GoogleReviews from "../home/GoogleReviews/GoogleReviews";
+import QuestionAnswer from "../Default Pages/QuestionAnswer/questionAnswer";
+import Footer from "../Default Pages/footer";
 
 const MainPageContact = () => {
   return (
-    <div>
-         <motion.div variants={fadeInUp}  whileInView="visible" viewport={{ once: false, amount: 0.2 }} className="opacity-0"> <Faces/></motion.div>
-         <motion.div variants={fadeInUp}  whileInView="visible" viewport={{ once: false, amount: 0.2 }} className="opacity-0"><Families/></motion.div>
-         <motion.div variants={fadeInUp}  whileInView="visible" viewport={{ once: false, amount: 0.2 }} className="opacity-0"> <ClientSay/></motion.div>
-         <motion.div variants={fadeInUp}  whileInView="visible" viewport={{ once: false, amount: 0.2 }} className="opacity-0"><Connected/></motion.div>
-         <motion.div variants={fadeInUp}  whileInView="visible" viewport={{ once: false, amount: 0.2 }} className="opacity-0">   <QuestionAnswer onSet={"agent"}/></motion.div>
-         <motion.div variants={fadeInUp}  whileInView="visible" viewport={{ once: false, amount: 0.2 }} className="opacity-0">  <Footer/></motion.div></div>
-  )
-}
+    <div className="font-dmsans">
 
-export default MainPageContact
+      <Faces />
+
+      <Families />
+
+      <GoogleReviews />
+
+      <Connected />
+
+      <QuestionAnswer onSet="agent" />
+
+      <Footer />
+
+    </div>
+  );
+};
+
+export default MainPageContact;
