163c43f798a3b927a53af0c268c9dbee0e9dd40a
Big Fan Bro <rajatsharma270@gmail.com>
2026-07-21 19:39:35 +0530

    2

--- src/component/page/resources/firstPageResource.js
+++ src/component/page/resources/firstPageResource.js
@@ -27,7 +27,19 @@ const FirstPageResource = () => {
 
      {/* Header */}
 
-<div className="max-w-3xl mx-auto text-center">
+<div
+  className="
+    relative
+    z-10
+    max-w-7xl
+    mx-auto
+    px-5
+    lg:px-8
+    py-10
+    md:py-16
+    lg:py-20
+    font-Montserrat
+">
 
   <p className="uppercase tracking-[0.35em] text-[#A61E22] text-sm font-semibold">
     RESOURCE CENTER
